be213e1026d96f003055649ef77cecd83ed09715
ellengoddard <ellenamaliaa@gmail.com>
2024-03-02 17:19:10 +0000

    test: add passing test for checking the mors_dfs_dict

--- func_mors_dfs.py
+++ func_mors_dfs.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from pandas import DataFrame
-from pandas import StringDtype
 
 # Filter and remove Baby Steps rows from MORS dataset
 def filter_bab(mors_df: DataFrame) -> DataFrame :
@@ -8,9 +7,15 @@ def filter_bab(mors_df: DataFrame) -> DataFrame :
     return mors_df_filtered
 
 # Split MORS into PRS, PTT, and COS dataframes
-# def mors_dfs(mors_df: DataFrame) -> dict[str, DataFrame] :
-#     assessment_tools = mors_df_filtered["assessment_tool"].unique()
-#     mors_dfs = {assessment_tool: [mors_df['assessment_tool'] == assessment_tool] for assessment_tool in assessment_tools}
-#     return mors_dfs
+def create_mors_df_dict(mors_df: DataFrame) -> dict[str, DataFrame]:
+    # Initialise the dictionary
+    mors_df_dict = {}
 
+    assessment_tools = mors_df["assessment_tool"].unique()
 
+    for tool in assessment_tools:
+        filtered_df = mors_df[mors_df['assessment_tool'] == tool]
+        # Update the dictionary:
+        mors_df_dict[tool] = filtered_df
+
+    return mors_df_dict
