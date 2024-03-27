02712ee79d0537ce1b699e79fb39fb0483f78814
ellengoddard <ellenamaliaa@gmail.com>
2024-03-27 20:12:17 +0000

    add failing test for string to date helper function

--- helper_functions.py
+++ helper_functions.py
@@ -1,3 +1,4 @@
+from datetime import date
 def reduce_df_dict(df_dict, function):
     '''
     Iterates over a dictionary of DataFrames, applying a given function to each
@@ -6,7 +7,7 @@ def reduce_df_dict(df_dict, function):
     Parameters:
         df_dict (dict): A dictionary where keys are identifiers and values are pandas DataFrames.
         function (callable): A function that takes a DataFrame and one argument.
-        
+
     Returns:
         dict: A dictionary with the same keys as `df_dict` but with the values being the transformed
               DataFrames.
@@ -23,3 +24,7 @@ def reduce_df_dict(df_dict, function):
     transformed_dict = dict(zip(dict_keys, transformed_values))
 
     return transformed_dict
+
+def parse_mors_datestring(datestring) -> date:
+    pass
+    # date.strftime(%)
