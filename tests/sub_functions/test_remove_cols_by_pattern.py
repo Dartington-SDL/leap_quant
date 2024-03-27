53b93dec3d4fe8a4b7eb927ce5a9116febf671a6
ellengoddard <ellenamaliaa@gmail.com>
2024-03-27 12:59:15 +0000

    add test to drop cols

--- tests/sub_functions/test_remove_cols_by_pattern.py
+++ tests/sub_functions/test_remove_cols_by_pattern.py
@@ -1,5 +1,41 @@
-def func(x):
-    return x + 1
+# Remove individual quesrtion response columns from the aggregated dataframes 
+import pandas as pd 
 
-def test_answer():
-    assert func(3) == 4
+def remove_cols_by_pattern(df):
+    '''
+    Returns dataframe without question response columns 
+
+        Parameters:
+            df (dataframe): Dataframe with aggragated scores
+
+        Returns: 
+            df (dataframe): Dataframe without question response columns
+    '''
+
+def test_drops_correct_cols():
+    # arrange
+    raw_df = pd.DataFrame({
+    'id': ["1", "2", "3", "4", "5"],
+    'question_1': [4, 1, 10, 5, 8], 
+    'question_2': [4, 8, 11, 9, 3],
+    'question_3': [6, 4, 0, 7, 22],
+    'agg_score_1': [20, 45, 88, 90, 12],
+    'agg_score_2': [56, 55, 29, 99, 19],
+    'agg_score_3': [95, 37, 4, 29, 57]
+    })
+
+    # act
+    new_df = remove_cols_by_pattern(raw_df)
+
+    # assert
+    expected_df = pd.DataFrame({
+    'id': ["1", "2", "3", "4", "5"],
+    'agg_score_1': [20, 45, 88, 90, 12],
+    'agg_score_2': [56, 55, 29, 99, 19],
+    'agg_score_3': [95, 37, 4, 29, 57]
+    })
+    
+
+    assert pd.testing.assert_frame_equal(new_df, expected_df)
+
+
