8d8611fdf863db338958626cc7a34b17a7905e1a
ellengoddard <ellenamaliaa@gmail.com>
2024-03-28 23:39:31 +0000

    comment out logger

--- subfunctions/sub_func_transform_column_values.py
+++ subfunctions/sub_func_transform_column_values.py
@@ -1,10 +1,22 @@
 import pandas as pd
-def transform_column_values(df: pd.DataFrame, column_names: list[str], function: callable) -> pd.DataFrame :
+import logging
+
+logger = logging.getLogger(__name__)
+
+def transform_column_values(df: pd.DataFrame, column_names: list[str], function: callable, **kwargs) -> pd.DataFrame :
 
     copied_df = pd.DataFrame.copy(df)
     for column_name in column_names:
-
-        copied_df[column_name] = copied_df[column_name].apply(function)
+        try:
+            copied_df[column_name] = copied_df[column_name].apply(function)
+        except Exception:
+            pass
+            # name_in_kwargs = kwargs.get('name', 'Unknown')
+            # logger.warning(f"Failed to apply function {function.__name__} to a value in column {column_name} in DataFrame {name_in_kwargs}. Error: {e} - is the column missing?")
+        else:
+            pass
+            # name_in_kwargs = kwargs.get('name', 'Unknown')
+            # logger.warning(f"Failed to apply function {function.__name__} to a value in column {column_name} in DataFrame {name_in_kwargs} - does a row have a blank cell?")
 
     return copied_df
         
