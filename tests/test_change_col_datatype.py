8a73b9c012a06e78419363a68e099fa52c6059f9
ellengoddard <ellenamaliaa@gmail.com>
2024-03-28 23:39:37 +0000

    improve test

--- tests/test_change_col_datatype.py
+++ tests/test_change_col_datatype.py
@@ -1,24 +1,27 @@
 import pandas as pd
-
+from ..helper_functions import map_dataframe_dtypes
 
 def test_change_col_datatype():
     raw_df = pd.DataFrame({
         "name": ["Tom", "Dick", "Harry"],
         "age": [22, 37, 66],
-        "is_parent": ["True", "False", "True"]
+        "is_parent": [True, False, True],
+        "actual_bool": [True, False, True]
     })
 
     assert raw_df["age"].dtype == "int64"
-    assert raw_df["is_parent"].dtype == "object"
+    assert raw_df["is_parent"].dtype == "bool"
+    assert raw_df["actual_bool"].dtype == "bool"
 
     dtype_map = {
         "name": "object",
-        "age": "int64" ,
-        "is_parent": "bool"
+        "age": "int32" ,
+        "is_parent": "bool",
+        "actual_bool": "bool"
     }
+    
     mapped_dtype_df: pd.DataFrame = map_dataframe_dtypes(raw_df, dtype_map)
 
     assert mapped_dtype_df["name"].dtype == "object"
-    assert mapped_dtype_df["age"].dtype == "int64"
-    assert mapped_dtype_df["is_parent"].dtype == "bool"
+    assert mapped_dtype_df["age"].dtype == "int"
 
