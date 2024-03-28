a8fb42580ed6ecb286da270bdd136f50726e79c9
ellengoddard <ellenamaliaa@gmail.com>
2024-03-28 21:27:59 +0000

    comment out unused code

--- subfunctions/sub_func_check_datatypes.py
+++ subfunctions/sub_func_check_datatypes.py
@@ -1,13 +1,9 @@
-import pandas as pd
-from typing import Dict, Any
+# import pandas as pd
+# from typing import Dict, Any
 
-def check_datatypes(df: pd.DataFrame, column_datatype_dict: Dict[str, Any]):
-    copied_df = pd.DataFrame.copy(df)
-    column_names = column_datatype_dict.keys()
-    column_datatypes = column_datatype_dict.values()
+# def check_datatypes(df: pd.DataFrame, column_datatype_dict: Dict[str, Any]):
+#     copied_df = pd.DataFrame.copy(df)
+#     column_names = column_datatype_dict.keys()
+#     column_datatypes = column_datatype_dict.values()
 
-    dtypes = copied_df.dtypes.to_dict()
-
-  
-
-
+#     dtypes = copied_df.dtypes.to_dict()
